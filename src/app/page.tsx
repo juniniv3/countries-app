f8eaaa4cd7d97c0a6c819493d94be891f5222882
Junini Florez <juniniprogramador@outlook.com>
2023-10-16 18:28:16 -0500

    feat: added first svg worldmap component version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
-import Image from 'next/image'
+import { gql } from "@apollo/client";
+import { getClient } from '../../lib/graphql-client';
+import SvgWorldComponent from '@/components/Worldmap/Worldmap'
+const query = gql`query Now {
+  countries {
+    code
+    name
+    native
+    phone
+    currency
+    emoji
+    emojiU
+  }
+}`;
 
-export default function Home() {
-  return (
-    <h1>Hello World</h1>
-  )
+
+export default async function Home() {
+  const { data } = await getClient().query({ query });
+  console.log(data)
+  return (<>
+      <h1>Hello World {data.countries[0].name}</h1>
+      <h2>mapa</h2>
+      <SvgWorldComponent/>
+    </>)
 }
